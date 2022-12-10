291ca9daeb1bc1fd52408ce91cd672bc5d6c9449
“Sami-Souissi” <samisouissi708@gmail.com>
2022-12-10 14:06:02 +0100

    appv7.6.4_perf

--- app/main/sw.js
+++ app/main/sw.js
@@ -1,48 +1,58 @@
-//A2HS
+var APP_PREFIX = 'ECJE'     // Identifier for this app (this needs to be consistent across every cache update)
+var VERSION = 'version_07'              // Version of the off-line cache (change this value everytime you want to update cache)
+var CACHE_NAME = APP_PREFIX + VERSION
+var URLS = [                            // Add URL you want to cache in this list.
+  '/SampleWebsite/app/main/',                     // If you have separate JS/CSS files,
+  '/SampleWebsite/app/main/index.html'            // add path to those files here
+]
 
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-      caches.open('fox-store').then((cache) => cache.addAll([
-        '/app/main/index.html',
-        '/app/main/about.html',
-        '/app/main/reile.html',
-        '/app/main/story.html',
-        '/app/main/story-2.html',
-        '/app/main/style.css',
-        '/app/main/favicon.png',
-        '/app/main/ecje+2.mp4',
-        '/app/main/index.js',
-        '/app/main/story.mp4',
-        //assets
-          //css
-        '/app/main/assets/css/common.css ',
-        '/app/main/assets/css/jquery.fullPage.css',
-        '/app/main/assets/font/*',
-        '/app/main/assets/img/*',
-        '/app/main/assets/js/*',
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+  console.log('fetch request : ' + e.request.url)
+  e.respondWith(
+    caches.match(e.request).then(function (request) {
+      if (request) { // if cache is available, respond with cache
+        console.log('responding with cache : ' + e.request.url)
+        return request
+      } else {       // if there are no cache, try fetching request
+        console.log('file is not cached, fetching : ' + e.request.url)
+        return fetch(e.request)
+      }
 
+      // You can omit if/else for console.log & put one line below like this too.
+      // return request || fetch(e.request)
+    })
+  )
+})
 
+// Cache resources
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log('installing cache : ' + CACHE_NAME)
+      return cache.addAll(URLS)
+    })
+  )
+})
 
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      // `keyList` contains all cache names under your username.github.io
+      // filter out ones that has this app prefix to create white list
+      var cacheWhitelist = keyList.filter(function (key) {
+        return key.indexOf(APP_PREFIX)
+      })
+      // add current cache name to white list
+      cacheWhitelist.push(CACHE_NAME)
 
-
-
-
-
-
-
-
-
-
-
-        
-        
-      ])),
-    );
-  });
-  
-  self.addEventListener('fetch', (e) => {
-    console.log(e.request.url);
-    e.respondWith(
-      caches.match(e.request).then((response) => response || fetch(e.request)),
-    );
-  });
+      return Promise.all(keyList.map(function (key, i) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          console.log('deleting cache : ' + keyList[i] )
+          return caches.delete(keyList[i])
+        }
+      }))
+    })
+  )
+})
